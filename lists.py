75cd6640ca525e805b92e3359c4e1d32ef1f574c
jazminkugler <jkugler@mail.austral.edu.ar>
2025-05-09 11:03:31 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,16 @@
-# Replace the "ANSWER HERE" with your answer
-
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
-
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+def remove_elements(lista_to_remove_elements):
+    len_lista = len(lista_to_remove_elements)
+    if len_lista >= 6:
+        del lista_to_remove_elements [5]
+        del lista_to_remove_elements [4]
+        del lista_to_remove_elements [0]
+        return lista_to_remove_elements
+    elif len_lista  == 5:
+        del lista_to_remove_elements [4]
+        del lista_to_remove_elements [0]
+        return lista_to_remove_elements
+    elif len_lista == 2 or len_lista == 3 or len_lista == 4:      
+        del lista_to_remove_elements [0]
+        return lista_to_remove_elements
+    else:
+        return []
